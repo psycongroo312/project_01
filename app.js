263c94245192735ed2b952eae3981c13620a18d2
deni <deniphoto@mail.ru>
2023-04-25 17:36:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+///firstName = 'сок'
+///const lastName = 'Kurisu'
+///let age = 21
+
+//name = 'Deni'
+//console.log('Имя человека ' +name+', а возраст человека '+ age )
+////console.log(age)
+
+///const lastName = prompt('Введите фамилию')
+//alert(lastName + ' ' + firstName)
+
+// 3 Операторы 
+
+//let currentYear = 2023 
+//const birthYear = 2001 
+ 
+///const age = currentYear - birthYear
+
+// const a = 90 
+// const b = 100
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(++currentYear)
+// console.log(currentYear++)
+
+///4 Типы данных
+
+// const isprogrammer = true
+// const name = 'Deni'
+// const age = 21
+// console.log(typeof isprogrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+
+///5 Приоритет операторов.... 
+
+
+
+
+
+// 17.04.23
+// пятый урок
+
+// const age = 21;
+// let number = 5; 
+
+// number = 10;
+
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+
+// console.log(obj);
+
+// 18.04.23
+// шестой урок #типы данных
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+//  const bool = true;
+
+// объект
+
+// // const obj = {
+//     name: 'Deni',
+// //     age: 21,
+// //     weight: 77,
+// //     height: 191,
+// //     isMarried: false
+// // };
+
+// console.log(obj.name);
+
+// массив
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[4]);
+
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?','');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}`);
+
+// const user = 'Deni';
+
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + " - object");
+ 
+
+// гит 
+// сначала пишем "git init" чтобы гит следил именно за этой папкой 
+// "git all" сокращенно "git a" добавление всех файлов 
+// чтобы добавить один конкретный файл пишем "git add ."
+// для проверки пишем "git status"
+// git commit -a -m"first commit" - служит контрольной точкой. first commit это название 
